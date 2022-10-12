1d540b0007858e17a3decc653006cf729a0411f9
leey611 <leey611@newschool.edu>
2022-10-12 11:26:14 -0400

    ADD: enable zoom in zoom out ability on map

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ const svg = d3.selectAll("#nyc-zipcode-map").attr("viewBox", [0,0,width,height])
 
 const g = svg.append("g").attr("id", "path-group")
 
+let zoom = d3.zoom()
+    .scaleExtent([0.2, 2])
+    .on('zoom', handleZoom)
+
 makeMap('nyc-zip-code.geojson')
 getNewYorkData(nyc_data_url)
+
 
 //https://github.com/EP-Visual-Design/COVID-19-parsed-data/blob/main/c_data/nyc/c_subs/Brooklyn/c_meta.json
 //loadData('https://jht1493.net/COVID-19-Impact/Dashboard/a0/c_data/nyc/c_subs/Brooklyn/c_meta.json')
@@ -32,8 +37,9 @@ async function makeMap(url) {
         type: 'FeatureCollection',
         features: brooklyn_data
     }
-    let center_bk = path.centroid(brooklyn_geojson)
-    let projection = d3.geoMercator().scale(140000).center(center_bk).translate([width/2,height/2])//.fitExtent([[50,50],[width-margin[0], height-margin[1]]], geojson)
+    let center_bk = path.centroid(geojson)
+    let projection = d3.geoMercator().scale(140000).center(center).translate([width/2,height/2])//.fitExtent([[50,50],[width-margin[0], height-margin[1]]], geojson)
+    // .fitSize([width,height], geojson)
     let geoGenerator = d3.geoPath().projection(projection);
     
     // append paths
@@ -42,10 +48,22 @@ async function makeMap(url) {
         .enter()
         .append('path')
         .attr('d', geoGenerator);
+    // init Zoom-in Zoom-out
+    initZoom()
+    
 }
 
 async function getNewYorkData(url) {
     const data = await loadData(url)
     const current_date = data.c_dates[data.c_dates.length-1]
     console.log(current_date)
+}
+
+function initZoom() {
+    d3.select('svg').call(zoom)
+}
+
+function handleZoom(e) {
+    d3.select('svg g')
+        .attr('transform', e.transform)
 }
